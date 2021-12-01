9d79bff3bc5ce15216360aae4ea6fb134becc3fe
Jeremee Bornstein <jeremee.bornstein@gmail.com>
2021-12-01 12:26:47 -0500

    added ability for JS files to be accessed for use with jquery

---
+++ public/script.js
@@ -0,0 +1,5 @@
+$header = $('h1')
+
+$header.html('Still working')
+
+console.log('working')
--- server.js
+++ server.js
@@ -3,17 +3,25 @@ const express = require('express')
 const mongoose = require('mongoose')
 const productsRouter = require('./controllers/products')
 const app = express();
+const methodOverride = require('method-override')
+
+//remove after done with nodemon
+require('dotenv').config()
 
 //env constants
 const PORT = process.env.PORT
 const MONGODB_URL = process.env.MONGODB_URL
 
 //middleware
+
+app.use(express.urlencoded({extended:true}))
+app.use(methodOverride('_method'))
+app.use('/public', express.static('public'))
+
 //appRouter
 app.use('/',productsRouter)
 
 //open port
-
 app.listen(PORT, () => console.log(`Port ${PORT} is open`))
 
 
@@ -21,7 +29,7 @@ app.listen(PORT, () => console.log(`Port ${PORT} is open`))
 
 mongoose.connect(MONGODB_URL)
 
-db = mongoose.connection
+const db = mongoose.connection
 
 db.on('connected', () => console.log('MongoDB server connected'))
 db.on('disconnected', () => console.log('MongoDB has been disconnected'))
