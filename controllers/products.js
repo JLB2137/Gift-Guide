92d90e20369ee59071e409116a03ea283332f614
Jeremee Bornstein <jeremee.bornstein@gmail.com>
2021-12-01 11:44:07 -0500

    Added additional router information

--- controllers/products.js
+++ controllers/products.js
@@ -2,3 +2,8 @@ const express = require('express')
 
 const productRouter = express.Router() 
 
+productRouter.get('/', (req,res) => {
+    res.render('index.ejs')
+})
+
+modules.export = productRouter
