ffd5c75c43612fb08de85d66e3ee820c79ce6cb8
Jeremee Bornstein <jeremee.bornstein@gmail.com>
2021-12-04 23:01:49 -0500

    switching machines

---
+++ public/scripts/api.js
@@ -0,0 +1,8 @@
+
+const URL = 'https://serpapi.com/playground?q=Apple&tbm=isch&ijn=0'
+const API_KEY = 'ae1778046f5540190b367e434568f827315065f246b3ade6a0f8fee3eb22dd71'
+$
+
+function imagesAPI() {
+    promise = $.ajax(`${URL}`)
+}
