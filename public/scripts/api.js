ed301d43620b6f521f4de6b8bc37aaeef58b929d
Jeremee <jeremee.bornstein@gmail.com>
2021-12-10 00:46:13 -0500

    fixed public js files

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -1,12 +1,7 @@
 const $img = $('img')
-const $nextImg = $('#img-next')
-const $returnImg = $('#img-return')
 const $imgURL = $('#imgURL')
 const $imgUpdate = $('#img-update')
-const imgSrc = $img.attr('src')
-let promise = ''
-let searchTerm = ''
-let counter = 0
+
 
 
 $imgUpdate.on('click', function(evt) {
